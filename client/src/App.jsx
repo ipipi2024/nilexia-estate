226ede97c815c965186237687a7435e9aaba6116
ipipi2024 <ipulepipi60@mail.com>
2024-09-03 15:06:30 -0400

    added footer component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,6 +10,7 @@ import CreateListing from "./pages/CreateListing"
 import UpdateListing from "./pages/UpdateListing"
 import Listing from "./pages/Listing"
 import Search from "./pages/Search"
+import FooterCom from "./components/Footer"
 
 export default function App() {
   return (
@@ -27,8 +28,8 @@ export default function App() {
         <Route path='/sign-in' element={<SignIn />} />
         <Route path="/listing/:listingId" element={<Listing/>}/>
         <Route path="/search" element={<Search/>}/>
-        
       </Routes>
+      <FooterCom/>
     </BrowserRouter>
   )
 }
